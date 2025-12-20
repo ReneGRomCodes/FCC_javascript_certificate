532cf06cd53ead10bfaa401fd09f93ac05db5802
ReneGRomCodes <renegromcodes@gmail.com>
2025-12-20 13:03:01 +0100

    Added trivia_bot.js

---
+++ trivia_bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+const botName = "Trivia Bot";
+const botLocation = "your PC";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact = favoriteLanguage + " was developed by Brendan Eich in 1995"
+console.log(codingFact)
+
+codingFact = favoriteLanguage + " was developed within 10 days for the Netscape 2 browser."
+console.log(codingFact)
+
+codingFact = favoriteLanguage + " is not the same as Java."
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
