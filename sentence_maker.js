e732a8e36dc4c037737950130ee2239a67ef95f9
ReneGRomCodes <renegromcodes@gmail.com>
2025-12-21 11:53:16 +0100

    Added sentence_maker.js

---
+++ sentence_maker.js
@@ -0,0 +1,23 @@
+let adjective = "purple";
+let noun = "toaster";
+let verb = "teleported";
+let place = "underground library";
+let adjective2 = "explosive";
+let noun2 = "pancake";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The "
+    + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory)
+
+adjective = "ancient";
+noun = "compass";
+verb = "whispered";
+place = "abandoned lighthouse";
+adjective2 = "fragile";
+noun2 = "vase";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The "
+    + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("Second story: " + secondStory)
