f199662842ec5b8ede573531700a92e3dbdb9c36
ReneGRomCodes <renegromcodes@gmail.com>
2026-01-06 13:37:38 +0100

    Added readme

--- confirm_ending_tool.js
+++ confirm_ending_tool.js
@@ -2,5 +2,5 @@ function confirmEnding(string1, string2) {
     const lenString1 = string1.length;
     const lenString2 = string2.length;
 
-    return string1.slice(lenString1-lenString2) === string2
+    return string1.slice(lenString1-lenString2) === string2;
 }
